116429b825ac988bad81ea5663d3892c6777ca74
Marian De Silva <mdsw84@gmail.com>
2025-11-12 13:24:33 +0100

    query init

---
+++ code_alongs/04_quering/sql/ingest_salaries.sql
@@ -0,0 +1,9 @@
+-- if not exists -> makes this code idempotent
+
+CREATE TABLE
+    IF NOT EXISTS data_jobs AS (
+        SELECT
+            *
+        FROM
+            read_csv_auto ('data/salaries.csv')
+    );
