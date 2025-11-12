a3668b8091d34df81aa34afb3bdb8f4465c4baf5
Marian De Silva <mdsw84@gmail.com>
2025-11-12 11:10:22 +0100

    Intro jokes

---
+++ code_alongs/03_sql_intro/queries.sql
@@ -0,0 +1,22 @@
+SELECT 
+    * 
+FROM 
+    funny_jokes;
+
+
+
+-- sort by rating ascending order
+SELECT 
+    * 
+FROM 
+    funny_jokes
+ORDER by
+    rating;
+    
+-- sort by rating descending order
+SELECT 
+    * 
+FROM 
+    funny_jokes
+ORDER by
+    rating;
