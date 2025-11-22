5d7c3c5e4506ad0c1b2ee66a7dc8eb5dea96ae98
Marian De Silva <mdsw84@gmail.com>
2025-11-22 19:06:35 +0100

    insert data

---
+++ excercises/exercise_0/introduction_to_sql/1_insert_data.sql
@@ -0,0 +1,41 @@
+INSERT INTO
+    course_content
+VALUES
+    (
+        'intro, query, ingest csv, CLI, table, select, filtering',
+        1,
+        'lecture'
+    ),
+    (
+        'CRUD, conditionals, sorting, funcktions, grouping, strings',
+        2,
+        'lecture'
+    ),
+    (
+        'temporal data, check, set operations',
+        3,
+        'lecture'
+    ),
+    (
+        'joins, subquery, python and duckdb',
+        4,
+        'lecture'
+    ),
+    (
+        'dlt, evidencs BI as code, views, CTE',
+        5,
+        'lecture'
+    ),
+    (
+        'windows functions, macros, sql injection',
+        6,
+        'lecture'
+    ),
+    ('exercise_0', 1, 'exercise'),
+    ('exercise_1', 2, 'exercise'),
+    ('exercise_2', 3, 'exercise'),
+    ('exercise_3', 4, 'exercise'),
+    ('lab_week_4', 4, 'lab'),
+    ('lab_week_5', 5, 'lab'),
+    ('lab_week_6', 6, 'lab'),
+    ('tenta', 6, 'exam');
