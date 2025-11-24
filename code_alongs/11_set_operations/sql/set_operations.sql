16c1ddd17407868ee39293ce344cca0f0a4be7b1
Marian De Silva <mdsw84@gmail.com>
2025-11-24 10:15:56 +0100

    set operations

---
+++ code_alongs/11_set_operations/sql/set_operations.sql
@@ -0,0 +1,45 @@
+SELECT
+    *
+FROM
+    synthetic.sales_jan
+UNION
+SELECT
+    *
+FROM
+    synthetic.sales_feb;
+
+SELECT
+    product_name,
+    amount
+FROM
+    synthetic.sales_jan
+UNION
+SELECT
+    product_name,
+    amount
+FROM
+    synthetic.sales_feb;
+
+SELECT
+    product_name,
+    amount
+FROM
+    synthetic.sales_jan
+UNION ALL
+SELECT
+    product_name,
+    amount
+FROM
+    synthetic.sales_feb;
+
+SELECT
+    product_name,
+    amount
+FROM
+    synthetic.sales_jan
+INTERSECT
+SELECT
+    product_name,
+    amount
+FROM
+    synthetic.sales_feb;
