f42bcc84d106d1721ba18ec8a1db49f939048a3a
Marian De Silva <mdsw84@gmail.com>
2025-11-24 14:11:41 +0100

    joins

--- code_alongs/12_joins/sql/joins.sql
+++ code_alongs/12_joins/sql/joins.sql
@@ -27,3 +27,40 @@ SELECT
 FROM
     plants p
     RIGHT JOIN plant_care pc ON p.plant_id = pc.plant_id;
+
+-- Achieve RIGHT JOIN with LEFT JOIN
+SELECT
+    p.plant_id,
+    p.plant_name,
+    p.type,
+    pc.water_schedule,
+    pc.sunlight
+FROM
+    plant_care pc
+    LEFT JOIN plants p ON p.plant_id = pc.plant_id;
+
+-- inner join
+-- i want know wich plants that have plant care
+-- not intrested in those without plant care
+SELECT
+    p.plant_id,
+    p.plant_name,
+    p.type,
+    pc.water_schedule,
+    pc.sunlight
+FROM
+    plant_care pc -- vänster
+    INNER JOIN plants p -- höger
+    ON p.plant_id = pc.plant_id;
+
+-- full join
+SELECT
+    p.plant_id,
+    p.plant_name,
+    p.type,
+    pc.water_schedule,
+    pc.sunlight
+FROM
+    plant_care pc -- vänster
+    FULL JOIN plants p -- höger
+    ON p.plant_id = pc.plant_id;
