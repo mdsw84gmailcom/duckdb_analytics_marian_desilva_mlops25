4bd8d824489d98db287754ff9bc194c22eb0d62d
Marian De Silva <mdsw84@gmail.com>
2025-11-25 19:36:58 +0100

    add company_size transformation test

--- excercises/excercise_1/data_transformations/sql/1_transform_salaries.sql
+++ excercises/excercise_1/data_transformations/sql/1_transform_salaries.sql
@@ -1,4 +1,4 @@
--- test transformation before create 
+-- test transformation employment type before create 
 SELECT DISTINCT
     employment_type,
     CASE
@@ -8,5 +8,17 @@ SELECT DISTINCT
         WHEN employment_type = 'FT' THEN 'Full time'
         ELSE employment_type
     END AS employment_type_clean
+FROM
+    salaries_raw;
+
+-- test transformation of company size before create
+SELECT DISTINCT
+    company_size,
+    CASE
+        WHEN company_size = 'S' THEN 'Small'
+        WHEN company_size = 'M' THEN 'Medium'
+        WHEN company_size = 'L' THEN 'Large'
+        ELSE company_size
+    END AS company_size_clean
 FROM
     salaries_raw;
